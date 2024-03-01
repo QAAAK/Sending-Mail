99a7df09785c24b00d832ae520a44dd1ee6d2e4e
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-03-01 12:53:40 +0300

    A simple version of sending messages to Lotus

---
+++ Sending messages from the client/SendEmail.java
@@ -0,0 +1,23 @@
+import org.apache.commons.mail.SimpleEmail; 
+import org.apache.commons.mail.Email; 
+
+public class SendEmail {  
+    public static void main(String[] args) {  
+        final String username = "your_username";  
+        final String password = "your_password";
+    // Создаем сообщение электронной почты  
+    SimpleEmail email = new SimpleEmail();  
+    email.setHostName("smtp.gmail.com");  
+    email.addRecipient(toAddress);  
+    email.setSmtpPort(465);  
+    email.setAuthenticator(new DefaultAuthenticator(username, password));  
+    email.setSSLOnConnect(true);  
+
+    try {  
+        // Отправляем сообщение  
+        email.send();  
+        System.out.println("Сообщение отправлено!");  
+    } catch (Exception ex) {  
+        ex.printStackTrace();  
+    }  
+}  
