0085fa5ad69064b1c1b7d44ac4eb0533a1474b82
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-06-25 11:08:38 +0300

    Create Display.java
    Display class

---
+++ AppDB/Display.java
@@ -0,0 +1,169 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonTextDisplay extends JFrame {
+    private JTextField textField;
+
+    public ButtonTextDisplay() {
+        super("App DataBase");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+
+        textField = new JTextField(150);
+        panel.add(textField);
+
+        JButton button_select = new JButton("Выбрать");
+        button_select.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText(textField.getText() + " " + button_select.getText());
+            }
+        });
+        panel.add(button_select);
+
+        JComboBox<String> comboBox = new JComboBox<>(new String[]{"Выбрать поле из списка","name", "age ", "phone_num"});
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedItem = (String) comboBox.getSelectedItem();
+                if (selectedItem != null && selectedItem != "Выбрать поле из списка") {
+                    textField.setText(textField.getText() + " " + selectedItem);
+                }
+            }
+        });
+        panel.add(comboBox);
+
+        JButton button_group = new JButton("Группировать по");
+        button_group.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText(textField.getText() + " " + button_group.getText());
+            }
+        });
+        panel.add(button_group);
+
+        JButton button_order = new JButton("Сортировать по");
+        button_order.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText(textField.getText() + " " + button_order.getText());
+            }
+        });
+        panel.add(button_order);
+
+        JButton button_from_table = new JButton("из таблицы");
+        button_from_table.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText(textField.getText() + " " + button_from_table.getText());
+            }
+        });
+        panel.add(button_from_table);
+
+        JComboBox<String> comboBoxTable = new JComboBox<>(new String[]{"Выбрать таблицу из списка","analyt_mob", "cvm", "credists"});
+        comboBoxTable.addActionListener(new ActionListener() { // 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedItemTable = (String) comboBoxTable.getSelectedItem();
+                if (selectedItemTable != null && selectedItemTable != "Выбрать таблицу из списка") {
+                    textField.setText(textField.getText() + " " + selectedItemTable);
+                }
+            }
+        });
+        panel.add(comboBoxTable);
+
+        JButton button_save = new JButton("Сохранить");
+        button_save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+
+                String textToSave = Query.getTextFieldText(textField);
+
+                System.out.println(Query.formatQuery(textToSave));
+
+                
+            }
+        });
+        panel.add(button_save);
+
+        add(panel);
+        pack();
+
+        setSize(400, 350);
+        setLocationRelativeTo(null);
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new ButtonTextDisplay().setVisible(true);
+        });
+    }
+}
+
+// class Buttons {
+    
+//     public static String isMap (String button) {
+        
+//         Map<String,String> Commands = new HashMap<String,String>();
+        
+//         Commands.put("       ", "SELECT");
+//         Commands.put("               ", "GROUP BY");
+//         Commands.put("              ", "ORDER BY");
+//         Commands.put("          ", "FROM");
+//         Commands.put("     ", "exit");
+        
+        
+//         if (Commands.get(button) == null) {
+            
+//             return "0";
+            
+//         }
+        
+//         return  Commands.get(button);
+//     }
+	
+	
+// 	public static String refactoringButton(String button) {
+		
+// 		String refactoring_button   = button.replace(" ", "");
+// 		refactoring_button = refactoring_button.substring(0, 1).toUpperCase() + refactoring_button.substring(1);
+		
+// 		return refactoring_button;
+// 	}
+    
+    
+
+// }
+
+
+class Query extends JFrame{
+
+    public static String getTextFieldText(JTextField textField) {
+
+    return textField.getText(); 
+
+    }
+
+    public static String formatQuery (String noRefactoringQuery) {
+
+
+        String refactoringQuery = noRefactoringQuery.replace("Выбрать", "SELECT");
+        refactoringQuery = refactoringQuery.replace("Группировать по", "GROUP BY");
+        refactoringQuery = refactoringQuery.replace("Сортировать по", "ORDER BY");
+        refactoringQuery = refactoringQuery.replace("из таблицы", "FROM");
+
+        return refactoringQuery;
+
+    }
+
+
+	
+    
+
+
+
+}
