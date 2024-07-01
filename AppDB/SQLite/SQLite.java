de3abc5fc5518995b354b943e38d4e3c4dcf2562
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-07-01 14:22:04 +0300

    Update SQLite.java

--- AppDB/SQLite/SQLite.java
+++ AppDB/SQLite/SQLite.java
@@ -1,6 +1,9 @@
 package SQLite;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.sql.*;
+import java.io.PrintWriter;
 
 /**
  *
@@ -17,7 +20,7 @@ public class SQLite {
 
 
         try {
-            String url = "jdbc:sqlite:C:\\sample-database-sqlite-1\\Chinook.db";
+            String url = "jdbc:sqlite:C:\\Users\\santalovdv\\Desktop\\Java\\sample-database-sqlite-1\\Chinook.db";
 
             conn = DriverManager.getConnection(url);
 
@@ -36,29 +39,43 @@ public class SQLite {
 
 
 
-    public static void queryToDB (String query, Connection conn) throws SQLException {
-
+    public static void queryToCSV(String query, Connection conn) throws SQLException, FileNotFoundException, UnsupportedEncodingException {
 
         Statement stmt = conn.createStatement();
-
         ResultSet result = stmt.executeQuery(query);
+        ResultSetMetaData metaData = result.getMetaData();
 
+        int columnsNumber = metaData.getColumnCount();
+        PrintWriter writer = new PrintWriter("C:\\Users\\santalovdv\\Desktop\\Лист Microsoft Excel.csv");
+
+        for (int i = 1; i <= columnsNumber; i++) {
+            writer.print(metaData.getColumnName(i)); // Записываем наименования колонок
+            if (i < columnsNumber) {
+                writer.print(",");
+            }
+        }
+        writer.println();
 
         while (result.next()) {
-            System.out.print(result.getString(1));
-            System.out.println(" " + result.getString(2));
+            for (int i = 1; i <= columnsNumber; i++) {
+                writer.print(result.getString(i));
+                if (i < columnsNumber) {
+                    writer.print(",");
+                }
+            }
+            writer.println();
         }
-
-
-
-
+        writer.close();
     }
 
-    public static void main(String[] args) throws SQLException {
 
 
 
-        queryToDB("SELECT * FROM Album", connect(conn));
+    public static void main(String[] args) throws SQLException, FileNotFoundException, UnsupportedEncodingException {
+
+
+
+        queryToCSV("SELECT * FROM Customer", connect(conn));
 
     }
 }
