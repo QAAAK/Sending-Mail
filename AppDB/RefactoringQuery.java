3dcc716aec86af1a8d316c49146d15dfa3c72152
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-07-02 13:06:15 +0300

    Add files via upload

---
+++ AppDB/RefactoringQuery.java
@@ -0,0 +1,20 @@
+package RefactoringQuery;
+
+public class RefactoringQuery {
+
+    public static String tableName;
+
+    public static void getNameTable(String nameTable) {
+
+         tableName = nameTable;
+
+    }
+
+    public static void getColumnName () {
+
+
+
+
+    }
+
+}
