09afc7954872cb884c74feca55c4a075584f1406
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-07-04 13:13:35 +0300

    Add files via upload

--- AppDB/ButtonTextDisplay.java
+++ AppDB/ButtonTextDisplay.java
@@ -1,86 +1,158 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.LayoutManager;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import  SQLite.*;
 import RefactoringQuery.*;
 import java.sql.Connection;
 
-
 public class ButtonTextDisplay extends JFrame {
     private JTextField textField;
     private Connection conn = null;
-    // public ArrayList<String> columns = new ArrayList<>();
+    private JComboBox<String> comboBox; // Store the table combo box
+    private JComboBox<String> columnsComboBox;
+
+    private JComboBox<String> columnsOrderComboBox;
+
+    private JButton buttonSave;
+    private JButton buttonGroup;
+    private JButton buttonAgain;
+
     public ButtonTextDisplay() throws SQLException {
-
         super("App DataBase");
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
 
-        textField = new JTextField(87);
+        textField = new JTextField(90);
         panel.add(textField);
 
+        // Get table names
         ArrayList<String> tableNameAsDB = SQLite.allNamesTables(SQLite.connect(conn = null));
         String[] tableNameArray = tableNameAsDB.toArray(new String[0]);
 
-        JComboBox<String> comboBox = new JComboBox<>(tableNameArray);
-        JComboBox<String> columnsComboBox = new JComboBox<>();
-
+        // Table selection combo box
+        comboBox = new JComboBox<>(tableNameArray);
         comboBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String selectedItem = (String) comboBox.getSelectedItem();
 
-                if (selectedItem != null && selectedItem != "Выберете таблицу из списка") {
-                    textField.setText(textField.getText() + " " + "Выбрать из таблицы " + " " + selectedItem );
-                    RefactoringQuery.getNameTable(selectedItem);
+                String selectedTable = (String) comboBox.getSelectedItem();
 
-                }
-            }
-        });
-        panel.add(comboBox);
-
-        comboBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String selectedItem = (String) comboBox.getSelectedItem();
-
-                if (selectedItem != null && selectedItem != "Выберите поле из списка") {
+                if (selectedTable != null && !selectedTable.equals("Выберете таблицу из списка")) {
+                    textField.setText("Выбрать из таблицы " + selectedTable + " поле (поля) ");
+                    RefactoringQuery.getNameTable(selectedTable);
+                    // Update columns combo box
                     try {
-                        ArrayList<String> columnNames = SQLite.allColumnsNameTable(SQLite.connect(conn = null), selectedItem);
+                        ArrayList<String> columnNames = SQLite.allColumnsNameTable(SQLite.connect(conn = null), selectedTable);
                         String[] columnsArray = columnNames.toArray(new String[0]);
 
-                        DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>(columnsArray);
-                        columnsComboBox.setModel(model); // Обновляем модель существующего выпадающего списка
-
-                        pack(); // Перепаковываем компоненты окна
+                        columnsComboBox.setModel(new DefaultComboBoxModel<>(columnsArray));
+                        columnsOrderComboBox.setModel(new DefaultComboBoxModel<>(columnsArray));
                     } catch (SQLException ex) {
                         ex.printStackTrace();
                     }
                 }
             }
         });
+        panel.add(comboBox);
+
+        // Column selection combo box
+        columnsComboBox = new JComboBox<>(new String[] {"Выберете поле из списка"});
+        columnsComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedColumn = (String) columnsComboBox.getSelectedItem();
+                if (selectedColumn != null && !selectedColumn.equals("Выберете поле из списка")) {
+                    // Update textField with column
+                    textField.setText(textField.getText() + " " + selectedColumn);
+                    RefactoringQuery.getColumnName(selectedColumn);
+                }
+            }
+        });
         panel.add(columnsComboBox);
 
 
+        buttonGroup = new JButton("Группировать полученные поля");
+        buttonGroup.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                RefactoringQuery.groupQuery();
+                textField.setText(textField.getText() + " " + buttonGroup.getText());
+
+            }
+        });
+        panel.add(buttonGroup);
+
+        columnsOrderComboBox = new JComboBox<>(new String[] {"Сортировать по"});
+        columnsOrderComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedColumnOrder = (String) columnsOrderComboBox.getSelectedItem();
+                if (selectedColumnOrder != null && !selectedColumnOrder.equals("Сортировать по")) {
+                    // Update textField with column
+                    textField.setText(textField.getText() + " " + selectedColumnOrder);
+
+                }
+            }
+        });
+        panel.add(columnsOrderComboBox);
+
+
+        buttonSave = new JButton("Сохранить");
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+
+                    SQLite save = new SQLite();
+                    String saveLocation = save.chooseSaveLocation();
+
+                    if (saveLocation != null) {
+
+                        System.out.println(RefactoringQuery.resultQuery());
+                        SQLite.queryToCSV(RefactoringQuery.resultQuery(), SQLite.connect(conn), saveLocation);
+                        textField.setText("Файл успешно сохранен. Пожалуйста, закройте приложение.");
+                    }
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } catch (UnsupportedEncodingException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        panel.add(buttonSave);
+
+        buttonAgain = new JButton("Заново");
+        buttonAgain.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                RefactoringQuery.again();
+                textField.setText("");
+
+            }
+        });
+        panel.add(buttonAgain);
 
 
 
-
+        // Add panel to frame
         add(panel);
         pack();
-
         setSize(1000, 300);
+        setResizable(false);
         setLocationRelativeTo(null);
-
     }
 
     public static void main(String[] args) {
-
         SwingUtilities.invokeLater(() -> {
             try {
                 new ButtonTextDisplay().setVisible(true);
@@ -90,88 +162,3 @@ public class ButtonTextDisplay extends JFrame {
         });
     }
 }
-
-// class Buttons {
-
-//     public static String isMap (String button) {
-
-//         Map<String,String> Commands = new HashMap<String,String>();
-
-//         Commands.put("       ", "SELECT");
-//         Commands.put("               ", "GROUP BY");
-//         Commands.put("              ", "ORDER BY");
-//         Commands.put("          ", "FROM");
-//         Commands.put("     ", "exit");
-
-
-//         if (Commands.get(button) == null) {
-
-//             return "0";
-
-//         }
-
-//         return  Commands.get(button);
-//     }
-
-
-// 	public static String refactoringButton(String button) {
-
-// 		String refactoring_button   = button.replace(" ", "");
-// 		refactoring_button = refactoring_button.substring(0, 1).toUpperCase() + refactoring_button.substring(1);
-
-// 		return refactoring_button;
-// 	}
-
-
-
-// }
-
-
-//class Query extends JFrame{
-//
-//
-//     public static String getTextFieldText(JTextField textField) {
-//
-//     return textField.getText();
-//
-//     }
-//
-//
-//
-//      public static String queryInDatabase (String query) {
-//
-//      String[] items = query.split(" ");
-//
-//      ArrayList<String> columns = new ArrayList<>(Arrays.asList(items));
-//
-//      String targetQuery = String.join(",", columns);
-//
-//      String refreshTargetQuery = targetQuery.replace(",GROUP,", " GROUP BY ");
-//      refreshTargetQuery = refreshTargetQuery.replace("ORDER,", " ORDER BY ");
-//      refreshTargetQuery = refreshTargetQuery.replace("SELECT,", "SELECT ");
-//      refreshTargetQuery = refreshTargetQuery.replace(", FROM,", " FROM ");
-//
-//
-//
-//      return  refreshTargetQuery;
-//
-//      }
-//
-//      public static String formatQuery (String noRefactoringQuery) {
-//
-//
-//        String refactoringQuery = noRefactoringQuery.replace("Выбрать", "SELECT");
-//        refactoringQuery = refactoringQuery.replace("Группировать по", "GROUP");
-//        refactoringQuery = refactoringQuery.replace("Сортировать по", "ORDER");
-//        refactoringQuery = refactoringQuery.replace("из таблицы", "FROM");
-//
-//        refactoringQuery = Query.queryInDatabase(refactoringQuery);
-//
-//        return refactoringQuery;
-//
-//    }
-
-
-
-
-//}
