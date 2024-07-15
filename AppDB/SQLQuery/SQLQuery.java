d7b86988c6ea6ce13b836292efdf6f674a1d16ba
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-07-15 10:41:20 +0300

    Add files via upload

--- AppDB/SQLQuery/SQLQuery.java
+++ AppDB/SQLQuery/SQLQuery.java
@@ -120,15 +120,15 @@ public class SQLQuery {
     }
 
 
-    public void about ()  {
-
-        JFrame jFrame = new JFrame();
-        JOptionPane.showMessageDialog(jFrame, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt\n" +
-                                                " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut\n" +
-                                                " aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore\n " +
-                                                "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt\n " +
-                                                "mollit anim id est laborum.\n ");
-    }
+//    public void about ()  {
+//
+//        JFrame jFrame = new JFrame();
+//        JOptionPane.showMessageDialog(jFrame, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt\n" +
+//                                                " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut\n" +
+//                                                " aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore\n " +
+//                                                "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt\n " +
+//                                                "mollit anim id est laborum.\n ");
+//    }
 
 
 
