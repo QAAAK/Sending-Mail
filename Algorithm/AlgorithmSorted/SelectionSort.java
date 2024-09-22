ba4824585f959b33aae966b762a9444b6ff2d4cd
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-09-22 19:47:03 +0300

    Update SelectionSort.java

--- Algorithm/AlgorithmSorted/SelectionSort.java
+++ Algorithm/AlgorithmSorted/SelectionSort.java
@@ -1,16 +1,34 @@
 
 class SelectionSort {
 
-    public static void selectionSort(int[] array) {
+        public static void selectionSort(int[] array) {
 
         for (int i = 0; i < array.length; i++) {
-
+            
+            int position = i;
+            int min = array[i];
+            
+            for (int j = i + 1; j < array.length; j++) {
+                
+                if (array[j] < min) {
+                    
+                    position = j;
+                    min = array[j];
+                    
+                }
+            }
+            
+            array[position] = array[i];
+            array[i] = min;
         }
-
-
-
-
-
     }
-
+    
+    public static void main (String[] args) {
+        
+        int[] arr = new int[]{2,7,1,3,2};
+        
+        selectionSort(arr);
+        
+        System.out.println(Arrays.toString(arr));       
+    }
 }
